4c7d4b1b8e859b7c6a32ee2c5a7d5fd4755a89fa
Miguel Prada <mapra99@gmail.com>
2023-01-29 18:35:10 -0500

    fix(purchase cart): dont show scrollbar if not needed (#83)

--- app/components/purchase-cart-modal/purchase-cart-modal.tsx
+++ app/components/purchase-cart-modal/purchase-cart-modal.tsx
@@ -7,7 +7,7 @@ const PurchaseCartModal = ({ cart, onClose, onCartRemoval }: PurchaseCartModalPr
     <div className="z-50 fixed top-0 left-0 right-0 bottom-0">
       <div className="relative h-full w-full p-6 sm:px-10">
         <div className="absolute top-0 left-0 w-full h-full -z-10 bg-black opacity-40"/>
-        <div className="max-w-6xl mx-auto mt-20 max-h-full overflow-scroll pb-20 box-border">
+        <div className="max-w-6xl mx-auto mt-20 max-h-full overflow-y-auto pb-20 box-border">
           { cart ? (
             <PurchaseCartList
               cart={cart}
